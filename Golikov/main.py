6e6552a12cb7f61ed7436e166d6c0dac3223ff69
akurashov <Alkurashov@gmail.com>
2024-11-26 10:54:22 +0400

    Добавлен ввод с помощью клавиши Enter

--- Golikov/main.py
+++ Golikov/main.py
@@ -1,7 +1,7 @@
 from opencage.geocoder import OpenCageGeocode
 from  tkinter import *
 
-from coordinates_web import label
+
 
 
 def get_coordinates(city, key):
@@ -18,19 +18,20 @@ def get_coordinates(city, key):
         return f"Возникла ошибка:{er}"
 
 
-def show_coordinates():
+def show_coordinates(event=None):
     city = entry.get()
     coordinates = get_coordinates(city, key)
-    label.config(text = f"Координаты города {city}: {coordinates}" )
+    label.config(text = f"Координаты города {city}:\n {coordinates}" )
 
 key = '8b4a33d6325641709f146b214f0c1c78'
 
 window = Tk()
 window.title("Координаты городов")
-window.geometry("300x400")
+window.geometry("350x200")
 
 entry = Entry()
 entry.pack()
+entry.bind("<Return>", show_coordinates)
 
 button = Button(text = "Получение координат", command=show_coordinates)
 button.pack()
